0c574c2b76f7f1e9c1e2df9808471e4ae8c4aa6c
Nathan Banks <nathantb3@gmail.com>
2018-05-04 23:03:03 +0100

    Alpha v0.0 Release (#1)
    Initial version

---
+++ KeyboardDisplay/KeyboardDisplay/Listener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KeyboardDisplay
+{
+    public static class Listener
+    {
+        public static string prevstate = "off";
+        public static string curstate = "off";
+
+
+
+
+    }
+
+
+
+}
---
+++ KeyboardDisplay/KeyboardDisplay/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace KeyboardDisplay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        Storyboard fadeInStoryboard = new Storyboard();
+        Storyboard fadeOutStoryboard = new Storyboard();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,0,0,1);
+            dispatcherTimer.Start();
+
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (Keyboard.GetKeyStates(Key.CapsLock) == KeyStates.Toggled)
+            {
+                Listener.prevstate = Listener.curstate;
+                Listener.curstate = "on";
+                if (Listener.prevstate != Listener.curstate)
+                {
+                    label1.Content = "On";
+                    ShowChange();
+                }
+            }
+            else if (!(Keyboard.GetKeyStates(Key.CapsLock) == KeyStates.Toggled))
+            {
+                Listener.prevstate = Listener.curstate;
+                Listener.curstate = "off";
+                if (Listener.prevstate != Listener.curstate)
+                {
+                    label1.Content = "Off";
+                    
+                    ShowChange();
+                }
+            }
+            //MessageBox.Show("hi");
+        }
+
+        private async void ShowChange()
+        {
+            Storyboard sb = this.FindResource("FadeIn") as Storyboard;
+            Storyboard.SetTarget(sb, this);
+            sb.Begin();
+            await Task.Delay(5000);
+            Storyboard sb2 = this.FindResource("FadeOut") as Storyboard;
+            Storyboard.SetTarget(sb2, this);
+            sb2.Begin();
+
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        public static class WindowsServices
+        {
+            const int WS_EX_TRANSPARENT = 0x00000020;
+            const int GWL_EXSTYLE = (-20);
+
+            [DllImport("user32.dll")]
+            static extern int GetWindowLong(IntPtr hwnd, int index);
+
+            [DllImport("user32.dll")]
+            static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+            public static void SetWindowExTransparent(IntPtr hwnd)
+            {
+                var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+                SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+
+}
+
