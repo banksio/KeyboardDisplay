6b7b66f5066156df5fd493411b25506a2ec0585c
Nathan Banks <nathantb3@gmail.com>
2018-12-10 23:26:47 +0000

    Work on settings dialog

---
+++ KeyboardDisplay/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KeyboardDisplay
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            //logic for logon startup here
+
+        }
+    }
+}
